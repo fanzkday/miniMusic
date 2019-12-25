c9640631046bd2530ef2abdc713a1b50e9fc22d7
范智阔 <fzk@choicesoft.com.cn>
2019-12-25 18:29:46 +0800

    组件songlist优化

--- lib/pages/music/widget/songlist.dart
+++ lib/pages/music/widget/songlist.dart
@@ -5,32 +5,26 @@ typedef Play = void Function(int value);
 
 class Songlist extends StatelessWidget {
   final int idx;
-
   final Play play;
 
   Songlist({this.play, this.idx}) : assert(play != null, idx != null);
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: musicStore.songlist
-          .asMap()
-          .map((i, item) {
-            return MapEntry(
-              i,
-              ListTile(
-                onTap: () {
-                  this.play(i);
-                },
-                leading: this.idx == i ? Icon(Icons.arrow_forward, color: Colors.lightBlueAccent) : Text('$i'),
-                title: Text(item['name']),
-                subtitle: Text(item['songer']),
-                trailing: Text('${item['duration']}'),
-              ),
-            );
-          })
-          .values
-          .toList(),
+    return ListView.builder(
+      itemCount: musicStore.songlist.length,
+      itemBuilder: (cxt, i) {
+        var item = musicStore.songlist[i];
+        return ListTile(
+          onTap: () {
+            this.play(i);
+          },
+          leading: this.idx == i ? Icon(Icons.arrow_forward, color: Colors.lightBlueAccent) : Text('$i'),
+          title: Text(item['name']),
+          subtitle: Text(item['songer']),
+          trailing: Text('${item['duration']}'),
+        );
+      },
     );
   }
 }
